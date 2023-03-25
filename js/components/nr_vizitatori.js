a7bdcaef6b44fd70125ac5a34d092b796c97f744
HaibaoM55 <96226102+HaibaoM55@users.noreply.github.com>
2023-03-25 17:45:49 +0200

    nr de vizitatori

--- js/components/nr_vizitatori.js
+++ js/components/nr_vizitatori.js
@@ -3,8 +3,5 @@ if(dev){
 }else{
     leURL = "https://suntunaiurit.org/php/nr_viz/"
 }
-if(document.cookie != 'visit:true'){
-    document.cookie = "visit:true; expires=Fri, 31 Dec 9999 23:59:59 GMT"
-    fetch(leURL);
-}
+fetch(leURL);
 $("nr_viz").innerText = $("nr_viz").innerText + " " + loadFile("/php/nr_viz/a.txt");
